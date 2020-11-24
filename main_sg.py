5d72aac68916a11b89637222b3abb0f01f1d1ef7
l <1430244438@qq.com>
2020-11-24 16:59:20 +0800

    add config file for param

--- main_sg.py
+++ main_sg.py
@@ -1,12 +1,18 @@
 from utils import tab_printer
 from sg_net import SGTrainer
-from parser_sg import parameter_parser
+from parser_sg import sgpr_args
+import sys
+import os
 
 def main():
     """
     Parsing command line parameters, reading data, fitting and scoring a SimGNN model.
     """
-    args = parameter_parser()
+    args = sgpr_args()
+    if len(sys.argv)>1:
+        args.load(sys.argv[1])
+    else:
+        args.load('./config/config.yml')
     tab_printer(args)
     trainer = SGTrainer(args)
     trainer.fit()
--- parser_sg.py
+++ parser_sg.py
@@ -1,53 +1,48 @@
-import argparse
+import yaml
+import os
+class sgpr_args():
+    def __init__(self):
+        #arch
+        self.keep_node=1
+        self.filters_1=64
+        self.filters_2=64
+        self.filters_3=32
+        self.tensor_neurons=16
+        self.bottle_neck_neurons=16
+        #train
+        self.graph_pairs_dir="/dir_of_graph_pairs"
+        self.p_thresh=3
+        self.batch_size=128
+        self.epochs=500
+        self.fold="00"
+        self.dropout=0
+        self.learning_rate=1e-3
+        self.weight_decay=5e-4
+        self.gpu=0
+        self.logdir="./logs"
+        #test
+        self.model=""
 
-def parameter_parser():
-    """
-    A method to parse up command line parameters.
-    The default hyperparameters give a high performance model without grid search.
-    """
-    parser = argparse.ArgumentParser(description = "Run SimGNN.")
-
-    parser.add_argument("--graph_pairs_dir", nargs = "?",
-                        default = "/media/work/data/kitti/odometry/semantic-kitti/5_20/graph_pairs_sem_5_20_ds/",
-                        help = "Folder with graph pair jsons.")
-    parser.add_argument("--p_thresh", type=int, default=3,
-                        help="Threshold for positive samples. Default is 3m.")
-    parser.add_argument("--fold", type=str, default="00",
-                        help="fold xx to eval. Default is 00.")
-    parser.add_argument("--epochs", type = int, default = 500,
-                        help = "Number of training epochs. Default is 500.")
-    parser.add_argument("--logdir",
-                        type=str,
-                        default="./logs",
-                        help="Output log directory. Default is ./logs.")
-    parser.add_argument("--model",
-                        type=str,
-                        default="",
-                        help="model.")
-    parser.add_argument("--keep-node",
-                        type = float,
-                        default=1,
-                        help="Randomly delete some nodes and edges. Default is 1")
-    parser.add_argument("--filters-1", type = int, default = 64,
-                        help = "Filters (neurons) in 1st convolution. Default is 64.")
-    parser.add_argument("--filters-2", type = int, default = 64,
-                        help = "Filters (neurons) in 2nd convolution. Default is 64.")
-    parser.add_argument("--filters-3", type = int, default = 32,
-                        help = "Filters (neurons) in 3rd convolution. Default is 32.")
-    parser.add_argument("--tensor-neurons", type = int, default = 16,
-                        help = "Neurons in tensor network layer. Default is 16.")
-    parser.add_argument("--bottle-neck-neurons", type = int, default = 16,
-                        help = "Bottle neck layer neurons. Default is 16.")
-    parser.add_argument("--batch-size", type = int, default = 128,
-                        help = "Number of graph pairs per batch. Default is 128.")
-    parser.add_argument("--dropout", type = float, default = 0,
-                        help = "Dropout probability. Default is 0.")
-    parser.add_argument("--learning-rate", type = float, default = 0.001,
-                        help = "Learning rate. Default is 0.001.")
-    parser.add_argument("--weight-decay", type = float, default = 5*10**-4,
-                        help = "Adam weight decay. Default is 5*10^-4.")
-    parser.add_argument("--gpu", type=int, default=0,
-                        help="GPU id to use. Default is 0.")
-    
- 
-    return parser.parse_args()
+    def load(self,config_file):
+        config_args=yaml.load(open(os.path.abspath(config_file)))
+        print(config_args)
+        #arch
+        self.keep_node=config_args['arch']['keep_node']
+        self.filters_1=config_args['arch']['filters_1']
+        self.filters_2=config_args['arch']['filters_2']
+        self.filters_3=config_args['arch']['filters_3']
+        self.tensor_neurons=config_args['arch']['tensor_neurons']
+        self.bottle_neck_neurons=config_args['arch']['bottle_neck_neurons']
+        #train
+        self.graph_pairs_dir=config_args['train']['graph_pairs_dir']
+        self.p_thresh=config_args['train']['p_thresh']
+        self.batch_size=config_args['train']['batch_size']
+        self.epochs=config_args['train']['epochs']
+        self.fold=config_args['train']['fold']
+        self.dropout=config_args['train']['dropout']
+        self.learning_rate=config_args['train']['learning_rate']
+        self.weight_decay=config_args['train']['weight_decay']
+        self.gpu=config_args['train']['gpu']
+        self.logdir=config_args['train']['logdir']
+        #evo
+        self.model=config_args['evo']['model']
